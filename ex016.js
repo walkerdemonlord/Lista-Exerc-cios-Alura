76b8268ae251865c57cfef5ad5d6dc74b0d29a94
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-06-30 16:21:51 -0300

    Atividade Número Secreto

---
+++ ex016.js
@@ -0,0 +1,18 @@
+let numeroSecreto = Math.floor(Math.random()*11);
+let numeroTentado = parseInt(prompt("Digite um número entre 1 e 10"));
+
+console.log(numeroSecreto);
+
+while (numeroTentado !== numeroSecreto) {
+
+    if (isNaN(numeroTentado) || numeroTentado >= 11 || numeroTentado <= 0) {
+        numeroTentado = parseInt(prompt('Valor incorreto. Tente novamente: '));
+    }
+    else if (numeroTentado < numeroSecreto) {
+        numeroTentado = parseInt(prompt('O número secreto é maior. Tente novamente: '));
+    }
+    else if (numeroTentado > numeroSecreto) {
+        numeroTentado = parseInt(prompt('O número secreto é menor. Tente novamente: '));
+    }
+
+}
